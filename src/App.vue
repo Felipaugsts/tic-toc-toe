0cd2a387659e8b2c7a373aff69a01418910d5902
Felipe Augusto Silva <felipeas@ciant.com>
2022-05-06 10:31:47 -0300

    feat: navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+<script>
+import Navbar from "../src/components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+<style>
+@import url("./assets/CSS/Global.css");
+html,
+body {
+  margin: 0px !important;
+  height: 1000px !important;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-inner">
+      <img class="img-logo" src="../assets/images/SWORD_Health_logo.svg" />
+      <p class="font-medium">
+        Frontend developer<br />
+        <span class="bold">Challenge</span>
+      </p>
+    </div>
+  </div>
+  >
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: "rgb(205, 205, 205)",
+    };
+  },
+
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+
+  unmounted() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+
+  methods: {
+    onScroll() {
+      if (window.scrollY > 150) {
+        this.color = "transparent";
+      } else if (window.scrollY < 100) {
+        this.color = "rgb(205, 205, 205)";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  background-color: v-bind("color");
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+}
+.navbar-inner {
+  height: 100px;
+  width: 1050px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.img-logo {
+  width: 190px;
+}
+</style>
